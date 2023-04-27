33d18da87658133a9e884044b1957da82d0d060c
Clement <clement.pinard.2013@gmail.com>
2023-04-27 14:46:17 +0200

    creation de la page d'accueil

---
+++ components/Login.js
@@ -0,0 +1,14 @@
+import styles from "../styles/Login.module.css";
+
+function Login() {
+  return (
+    <div>
+      <main className={styles.main}>
+        <div className={styles.logo}>LOGO</div>
+        <div>blabla et bouton</div>
+      </main>
+    </div>
+  );
+}
+
+export default Login;
