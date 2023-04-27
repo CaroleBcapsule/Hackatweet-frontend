1a11ecafc5336557be1f80a11305b71fb60cc7c0
Clement <clement.pinard.2013@gmail.com>
2023-04-27 17:49:19 +0200

    ajout des modals

--- components/Home.js
+++ components/Home.js
@@ -1,32 +1,31 @@
 //import styles from '../styles/Home.module.css';
-import Head from 'next/head';
-import { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import LastTweets from './LastTweets';
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import LastTweets from "./LastTweets";
 //import Tweets from './Tweets';
 //import Trends from './Trends';
 
 function Home() {
   const user = useSelector((state) => state.user.value);
 
-   //récupération des tweets de tous les utilisateurs
-   const [allTweets, setAllTweets] = useState([]);
+  //récupération des tweets de tous les utilisateurs
+  const [allTweets, setAllTweets] = useState([]);
 
-   useEffect(() => {
-      //  if (!user.token) {
-      //      return;
-      //  }
-           fetch(`http://localhost:3000/tweets/${user.token}`)
-             .then(response => response.json())
-             .then(data => {
-               setAllTweets(allTweets);
-             });
-         }, []);
-   
-    const tousTweets = allTweets.map((data, i) => {
+  useEffect(() => {
+    //  if (!user.token) {
+    //      return;
+    //  }
+    fetch(`http://localhost:3000/tweets/${user.token}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setAllTweets(allTweets);
+      });
+  }, []);
+
+  const tousTweets = allTweets.map((data, i) => {
     return <LastTweets key={i} {...data} />;
-     });
-
+  });
 
   return (
     <div>
@@ -35,9 +34,7 @@ function Home() {
       </Head>
       <div>composant tweet</div>
       <div>
-      <div className={styles.container}>
-        {tousTweets}
-        </div>
+        <div className={styles.container}>{tousTweets}</div>
       </div>
     </div>
   );
--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,24 @@
 import styles from "../styles/Login.module.css";
-import ReactDOM from "react-dom";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter } from "@fortawesome/free-solid-svg-icons";
 import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+import { Button, Modal } from "antd";
+import { useState } from "react";
 
 function Login() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div>
       <main className={styles.main}>
@@ -17,13 +31,38 @@ function Login() {
             <h2>Join Hackatweet today</h2>
           </div>
           <div>
-            <button type="button" className={styles.buttonSignUp}>
+            <Button
+              className={styles.buttonSignUp}
+              type="primary"
+              onClick={showModal}
+            >
               Sign Up
-            </button>
+            </Button>
+            <Modal
+              title="Basic Modal"
+              open={isModalOpen}
+              onOk={handleOk}
+              onCancel={handleCancel}
+            >
+              <SignUp />
+            </Modal>
+
             <p>Already have an account ?</p>
-            <button type="button" className={styles.buttonSignIn}>
+            <Button
+              className={styles.buttonSignIn}
+              type="primary"
+              onClick={showModal}
+            >
               Sign in
-            </button>
+            </Button>
+            <Modal
+              title="Basic Modal"
+              open={isModalOpen}
+              onOk={handleOk}
+              onCancel={handleCancel}
+            >
+              <SignIn />
+            </Modal>
           </div>
         </div>
       </main>
